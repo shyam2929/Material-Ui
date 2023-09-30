84b045f3bf70c6aeb9fc4385b91d36516b8017d1
Shyam Sunder <shyamssbgk29@gmail.com>
2023-10-01 05:19:43 +0530

     

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { Typography, AppBar, Button, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material";
+import { Typography, AppBar, Button, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container, CardContent } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
-import { makeStyles } from 'tss-react/mui';
+import useStyles from "./Styles";
 
-const useStyles = makeStyles()((theme) => ({
-     container: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(8, 0, 6)
-     }
-}));
-
+const cards =[1, 2, 3, 4, 5, 6, 7, 8, 9]
 const App = () => {
     const { classes } = useStyles();
 
@@ -18,7 +12,7 @@ const App = () => {
          <CssBaseline />
          <AppBar position="relative">
             <Toolbar>
-                <PhotoCamera />
+                <PhotoCamera className={classes.icon} />
                 <Typography variant="h6">
                     Photo Album 
                 </Typography>
@@ -26,14 +20,14 @@ const App = () => {
          </AppBar>
          <main>
             <div className={classes.container}>
-                <container maxwidth="sm">
+                <Container maxwidth="sm">
                     <Typography variant="h2" align="center" color="textprimary" guttterbottom="true">
                         Photo Album
                     </Typography>
                     <Typography variant="h5" align="center" color="textSecondary" paragrapgh="true">
                         Hello everyone This is a photo album which  i created for travelers to store their travel photos according to their trips into different albums
                     </Typography>
-                    <div>
+                    <div className={classes.buttons}>
                         <Grid container spacing={2} justifyContent="center">
                             <Grid item>
                                 <Button variant="contained" color="primary">
@@ -47,9 +41,45 @@ const App = () => {
                             </Grid>
                         </Grid>
                     </div>
-                </container>
+                </Container>
             </div>
+            <Container className={classes.cardGrid} maxwidth="md">
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Card className={classes.card}>
+                            <CardMedia 
+                                 className={classes.cardMedia}
+                                 image="https://source.unsplash.com/random"
+                                 title="Image Title"
+                            />
+                            <CardContent className={classes.cardContent}>
+                                <Typography gutterBottom variant="h5">
+                                  Heading  
+                                </Typography>
+                                <Typography>
+                                    This is a media card. This will be used for content description 
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">View</Button>
+                                <Button size="small" color="primary">Edit</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    ))}
+                    
+                </Grid>
+            </Container>
          </main>
+         <footer className={classes.footer}>
+            <Typography variant="h6" align="center" gutterBottom>
+                Home,    About,     Contact
+            </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary">
+                ©2023 Travel Diary, All Rights Reserved
+            </Typography>
+         </footer>
        </>
     );
 }
